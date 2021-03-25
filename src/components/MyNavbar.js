b8885e344ea6939ba97eff16699e1f0bcc3dd371
Anthony C <achian@ucsc.edu>
2021-03-25 03:25:21 -0700

    fixed grid and navbar height

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,16 +1,16 @@
 import React from 'react'
+import '../css/MyNavbar.css';
 
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 
 export default function MyNavbar() {
     return (
-        <>
-            <Navbar bg="dark" variant="dark">
+        <div>
+            <Navbar className="my-navbar" bg="dark" variant="dark">
                 <Navbar.Brand href="#home">Genshin</Navbar.Brand>
                     <Nav className="mr-auto">
                         <Nav.Link href="#home">Characters</Nav.Link>
@@ -22,6 +22,7 @@ export default function MyNavbar() {
                     <Button variant="outline-info">Search</Button>
                 </Form>
             </Navbar>
-        </>
+        </div>
     )
 }
+// style={{height: "10vh"}} 
